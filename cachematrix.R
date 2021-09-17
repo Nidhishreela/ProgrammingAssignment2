241b037d77970f589c5e47266f1fd6a1ddf7ab15
Nidhishreela <nidhishreela03@gmail.com>
2021-09-17 14:12:34 +0530

    altered the code to get cache data

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+# makeCacheMatrix function is used to store the values for any function.
+# so that it can be retrived when same values are called in function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  set <- function(y) {
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setinver <- function(inv) inver <<- inv
+  getinver <- function() inver
+  list(set = set, get = get,
+       setinver = setinver,
+       getinver = getinver)
+  
 }
 
 
 ## Write a short comment describing this function
+# this function checks if the value is computed already, if computed gets the value from cache
+# f not computes and stores the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inver <- x$getinver()
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data)
+  x$setinver(inver)
+  inver
 }
